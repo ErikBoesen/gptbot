6dcd2e7633b64b3712c38aa5affd50193701d93b
ErikBoesen <me@erikboesen.com>
2023-04-15 02:12:31 -0400

    Clean up bot code

--- handler.py
+++ handler.py
@@ -10,6 +10,26 @@ PREFIX = '>'
 OPENAI_ENDPOINT = 'https://api.gpt-3.5-turbo.com/query'
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
+def receive(event, context):
+    message = json.loads(event['body'])
+
+    bot_id = message['bot_id']
+    response = process_text(message)
+    if response:
+        send(response, bot_id)
+
+    return {
+        'statusCode': 200,
+        'body': 'ok'
+    }
+
+def process_message(message):
+    # Prevent self-reply
+    if message['sender_type'] != 'bot':
+        text = message['text']
+        if text.startswith(PREFIX):
+            return process_text(text)
+
 def process_text(text):
     headers = {
         'Content-Type': 'application/json',
@@ -28,26 +48,6 @@ def process_text(text):
     if response.status_code == 200:
         return response.json()['choices'][0]['text']
     return 'Error: ' + response.text
-
-def receive(event, context):
-    message = json.loads(event['body'])
-
-    bot_id = message['bot_id']
-    response = process_text(message)
-    if response:
-        send(response, bot_id)
-
-    return {
-        'statusCode': 200,
-        'body': 'ok'
-    }
-
-
-def process(message):
-    # Prevent self-reply
-    if message['sender_type'] != 'bot':
-        if message['text'].startswith(PREFIX):
-            return random.choice(OPTIONS)
 
 
 def send(text, bot_id):
